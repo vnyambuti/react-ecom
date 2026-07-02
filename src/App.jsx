2fe468d1cb9615e465fb41df254937fb6953cf81
vnyambuti <vnyambuti@gmail.com>
2026-07-02 12:30:28 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { HomePage } from './pages/HomePage'
+import { OrderPage } from './pages/OrderPage'
+import { TrackingPage } from './pages/TrackingPage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router'
+
+function App() {
+
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrderPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,88 @@
+
+import { HeaderComponent } from '../components/HeaderComponent';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import './HomePage.css';
+// import { products } from '../../starting-code/data/products.js'
+
+
+export function HomePage() {
+    const [products, setProductsData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProductsData(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching products:', error);
+            });
+    }, []);
+
+    return (
+        <>
+            <title>Home</title>
+            <HeaderComponent />
+
+            <div className="home-page">
+                <div className="products-grid">
+                    {products.map((product) => {
+                        return (
+                            <div className="product-container" key={product.id}>
+                                <div className="product-image-container">
+                                    <img className="product-image"
+                                        src={product.image} />
+                                </div>
+
+                                <div className="product-name limit-text-to-2-lines">
+                                    {product.name}
+                                </div>
+
+                                <div className="product-rating-container">
+                                    <img className="product-rating-stars"
+                                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+                                    <div className="product-rating-count link-primary">
+                                        {product.rating.count}
+                                    </div>
+                                </div>
+
+                                <div className="product-price">
+                                    ${(product.priceCents / 100).toFixed(2)}
+                                </div>
+
+                                <div className="product-quantity-container">
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                </div>
+
+                                <div className="product-spacer"></div>
+
+                                <div className="added-to-cart">
+                                    <img src="images/icons/checkmark.png" />
+                                    Added
+                                </div>
+
+                                <button className="add-to-cart-button button-primary">
+                                    Add to Cart
+                                </button>
+                            </div>
+                        );
+                    })}
+
+
+                </div>
+            </div>
+        </>
+    );
+}
